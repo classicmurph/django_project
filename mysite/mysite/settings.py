6a499e1cc598b29fc3bd8d1c4fdea3df08518bd0
Dean Fitzgerald <classicmurph@gmail.com>
2016-05-04 12:29:57 -0400

    upgraded to 1.9

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,13 +32,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -75,13 +75,6 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Internationalization
